e6ba4cc2f50d0b8945daf98e7a8acfedbda081b9
Thunder <111kyrie111@mail.ru>
2025-03-01 19:46:58 +0500

    Added info about running services

--- backend/info/main.py
+++ backend/info/main.py
@@ -3,7 +3,6 @@ import httpx
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 import os
 
 app = FastAPI()
@@ -17,25 +16,37 @@ app.add_middleware(
 )
 
 
-RELATED_SERVICES = os.getenv("RELATED_SERVICES", "").split(",")
+CONTAINERS_INFO = os.getenv("RELATED_SERVICES", "").split(",")
 
 @app.get("/")
 async def read_root():
-    ServiceName = []
-    if RELATED_SERVICES and RELATED_SERVICES[0]:
-        async with httpx.AsyncClient() as client:
-            for service_url in RELATED_SERVICES:
-                service_url = service_url.strip()  # Удаление пробелов
-                try:
-                    response = await client.get(service_url)
-                    data = response.json()
-                    ServiceName.append({"Service Name: " : data.get('Service_Name', 'Not found'), "Service_Version: ": data.get('Service_Version', 'Not found'), "Service Id: ":  data.get('id', 'Not found'), "Service URL: " : service_url})
-                    print(data)
-                except:
-                    print("Error")
-    else: print("No Services")
+    ServicesInfo = []
+    async with httpx.AsyncClient() as client:
+        for service_url in CONTAINERS_INFO:
+            service_url = service_url.strip()  # Удаление пробелов
+            try:
+                response = await client.get(service_url)
+                data = response.json()
+                ServicesInfo.append({"Service Name: " : data.get('Service_Name', 'Not found'),
+                                    "Service_Version: ": data.get('Service_Version', 'Not found'),
+                                    "Service Id: ":  data.get('id', 'Not found'),
+                                    "Service URL: " : service_url})
+                print(data)
+            except:
+                print("Can't get ServicesInfo")
+    try:
+        client = docker.from_env()
+        containers = client.containers.list()
+        for container in containers:
+            if (container.status == "running"):
+                ServicesInfo.append({"Is Running: " : "Yes"})
+            else:
+                ServicesInfo.append({"Is Running: " : "No"})
+    except:
+        print("Can't get Running containers info")    
 
-    return print(ServiceName)
+        
+    return print(ServicesInfo)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
